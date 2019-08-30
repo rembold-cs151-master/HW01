49a5d4fd507aa2ae6b4eced3419b58094fc72b72
Ethan Levitt <ethan@thenumberfortytwo.com>
2019-08-30 13:21:25 -0700

    Completed Problem 3

--- Prob3.py
+++ Prob3.py
@@ -1,6 +1,6 @@
 #---------------- Problem 3 ------------------
 #
-# Name:
+# Name: Ethan Levitt
 #
 # Who did you work with (if anyone)?:
 #
@@ -12,19 +12,32 @@ def big_odd_finder(): # Pls don't change this line, for my own purposes
     # these comments are (sorry, it has to do with my own purpose above)
 
     # Prompt for variable inputs
+    listOfOddNumbers = []
 
-
+    x= int(input("Input value of X:"))
+    y= int(input("Input value of Y:"))
+    z= int(input("Input value of Z:"))
 
     # Logic to check for being largest odd:
 
+    if x % 2 == 1:
+        listOfOddNumbers.append(x)
 
+    if y % 2 == 1:
+        listOfOddNumbers.append(y)
 
+    if z % 2 == 1:
+        listOfOddNumbers.append(z)
 
+    listOfOddNumbers.sort(reverse=True)
 
     # Print out largest odd value
     # or "No odd numbers found." if no odds exist
-
-
+    
+    if len(listOfOddNumbers) > 0:
+        print (listOfOddNumbers[0])
+    else:
+        print ("No odd numbers found")
 
 # Don't modify code below here please!
 if __name__ == '__main__':
