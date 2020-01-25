3e986515ce1e0bb70068f9e7fff6d3c33dfda321
Jed Rembold <jedediah.r@gmail.com>
2020-01-24 17:30:32 -0800

    Update unit tests for squares

--- test_hw1.py
+++ test_hw1.py
@@ -21,21 +21,35 @@ class Test_WrittenWork:
 
 
 class Test_Prob4:
-    def test_d_is_zero(self, capsys):
-        Prob4.find_area(10,5,0)
+    def test_B_big_d_zero(self, capsys):
+        Prob4.find_area(2,3,0)
         captured = capsys.readouterr()
-        assert numcheck(float(captured.out.rstrip()),50)
+        assert numcheck(float(captured.out.rstrip()),13)
 
-    def test_d_is_A(self, capsys):
-        Prob4.find_area(10,5,10)
+    def test_B_big_d_halfA(self, capsys):
+        Prob4.find_area(2,3,1)
         captured = capsys.readouterr()
-        assert numcheck(float(captured.out.rstrip()), 37.5)
+        assert numcheck(float(captured.out.rstrip()), 11)
 
-    def test_d_is_halfA(self, capsys):
+    def test_B_big_d_A(self, capsys):
+        Prob4.find_area(3,5,3)
+        captured = capsys.readouterr()
+        assert numcheck(float(captured.out.rstrip()), 25)
+
+    def test_A_big_d_zero(self, capsys):
+        Prob4.find_area(6,3,0)
+        captured = capsys.readouterr()
+        assert numcheck(float(captured.out.rstrip()),45)
+
+    def test_A_big_d_halfB(self, capsys):
+        Prob4.find_area(2,1,0.5)
+        captured = capsys.readouterr()
+        assert numcheck(float(captured.out.rstrip()), 4.5)
+
+    def test_A_big_d_B(self, capsys):
         Prob4.find_area(10,5,5)
         captured = capsys.readouterr()
-        assert numcheck(float(captured.out.rstrip()), 34.375)
-
+        assert numcheck(float(captured.out.rstrip()), 100)
 
 class Test_Prob3:
     def test_all_evens(self, capsys):
@@ -46,22 +60,41 @@ class Test_Prob3:
             assert cap.out == 'No odd numbers found.\n'
 
     def test_all_odds(self, capsys):
-        inputs = ['3', '-7', '1']
-        with mock.patch('builtins.input', side_effect=inputs):
-            Prob3.big_odd_finder()
-            cap = capsys.readouterr()
-            assert cap.out == '3\n'
+        inputs = [
+                ['3', '-7', '1'],
+                ['1', '-3', '7'],
+                ['-1', '5', '3']
+                ]
+        sols = ['3', '7', '5']
+
+        for _input,sol in zip(inputs,sols):
+            with mock.patch('builtins.input', side_effect=_input):
+                Prob3.big_odd_finder()
+                cap = capsys.readouterr()
+                assert cap.out.rstrip() == sol
 
     def test_one_odd(self, capsys):
-        inputs = ['-2', '-7', '8']
-        with mock.patch('builtins.input', side_effect=inputs):
-            Prob3.big_odd_finder()
-            cap = capsys.readouterr()
-            assert cap.out == '-7\n'
+        inputs = [
+                ['-2', '-7', '8'],
+                ['7', '8', '-4'],
+                ['4', '8', '11']
+                ]
+        sols = ['-7', '7', '11']
+        for _input, sol in zip(inputs,sols):
+            with mock.patch('builtins.input', side_effect=_input):
+                Prob3.big_odd_finder()
+                cap = capsys.readouterr()
+                assert cap.out.rstrip() == sol
 
     def test_two_odd(self, capsys):
-        inputs = ['11', '7', '8']
-        with mock.patch('builtins.input', side_effect=inputs):
-            Prob3.big_odd_finder()
-            cap = capsys.readouterr()
-            assert cap.out == '11\n'
+        inputs = [
+                ['11', '7', '8'],
+                ['3', '9', '2'],
+                ['20', '3', '13'],
+                ]
+        sols = ['11', '9', '13']
+        for _input, sol in zip(inputs,sols):
+            with mock.patch('builtins.input', side_effect=_input):
+                Prob3.big_odd_finder()
+                cap = capsys.readouterr()
+                assert cap.out.rstrip() == sol
